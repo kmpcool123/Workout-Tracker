046becd3312dafd81029eb1306377d40926616d8
arnold mulemba <arnoldmulemberi@gmail.com>
2021-05-15 15:41:23 -0400

    Update DataTable

--- Workout-Tracker/Workout-Tracker.Data/Workout.cs
+++ Workout-Tracker/Workout-Tracker.Data/Workout.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,26 @@ namespace Workout_Tracker.Data
 {
     public class Workout
     {
-    }
+            [Key]
+            public int WorkoutID { get; set; }
+
+            [Required]
+            public string NameOfWorkout { get; set; }
+
+            [Required]
+            public Guid UserID { get; set; }
+
+            [Required]
+            public string Description { get; set; }
+
+            [Required]
+            public DateTimeOffset CreatedUtc { get; set; }
+
+            public DateTimeOffset ModifiedUtc { get; set; }
+
+            [ForeignKey(nameof(Routine))]
+            public int RoutineID { get; set; }
+
+            public virtual ICollection<Routine> Routine { get; set; }
+      }
 }
