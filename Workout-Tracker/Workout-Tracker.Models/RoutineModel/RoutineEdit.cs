941f199e48273e8675d6537e8e3e5df22cda4124
Kayla-Kirk <Therainbowbitme@gmail.com>
2021-05-15 15:10:24 -0400

    pushing model for routine

---
+++ Workout-Tracker/Workout-Tracker.Models/RoutineModel/RoutineEdit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_Tracker.Models.RoutineModel
+{
+    class RoutineEdit
+    {
+        [Key]
+        public int RoutineID { get; set; }
+        public string NameofRoutine { get; set; }
+        public string Description { get; set; }
+    }
+}
