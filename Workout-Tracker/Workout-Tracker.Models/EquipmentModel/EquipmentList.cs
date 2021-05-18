ecf84ce26bf9fa0893b73b160be879958dc1f07f
samuelhana89 <78116893+samuelhana89@users.noreply.github.com>
2021-05-18 19:23:59 -0400

    model

---
+++ Workout-Tracker/Workout-Tracker.Models/EquipmentModel/EquipmentList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_Tracker.Models.EquipmentModel
+{
+    public class EquipmentList
+    {
+        [Display(Name="Equipment ID")]
+        public int EquipmentId { get; set; }
+
+        [Display(Name = "Equipment Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Time Duration")]
+        public DateTime TimeLenght { get; set; }
+    }
+}
