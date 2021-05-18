ff55c5450b51b70e34469467071751a9ba878671
kmpcool123 <60276295+kmpcool123@users.noreply.github.com>
2021-05-18 18:01:46 -0400

    Revert "Khalil/11/building controllers=exercise"

--- Workout-Tracker/Workout-Tracker.Data/Exercise.cs
+++ Workout-Tracker/Workout-Tracker.Data/Exercise.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +8,5 @@ namespace Workout_Tracker.Data
 {
     public class Exercise
     {
-        [Key]
-        public int ExerciseId { get; set; }
-        [Required]
-        public Guid UserId { get; set; }
-        [Required]
-        public string ExerciseName { get; set; }
-        [Required]
-        public string ExerciseDescription { get; set; }
-
-        [Required]
-        [ForeignKey("Routine")]
-        public int RoutineID { get; set; }
-        public virtual Routine Routine { get; set; }
-
-        [Required]
-        public DateTimeOffset CreatedUtc { get; set; }
-        public DateTimeOffset? ModifiedUtc { get; set; }
-
     }
 }
