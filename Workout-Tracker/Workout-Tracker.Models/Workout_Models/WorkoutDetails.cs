4eb207e9692857d9b12547e2427697d3b5078bc0
arnold mulemba <arnoldmulemberi@gmail.com>
2021-05-18 21:48:38 -0400

    Update Workout ModelLayer

--- Workout-Tracker/Workout-Tracker.Models/Workout_Models/WorkoutDetails.cs
+++ Workout-Tracker/Workout-Tracker.Models/Workout_Models/WorkoutDetails.cs
@@ -16,7 +16,7 @@ namespace Workout_Tracker.Models
             public string WorkoutName { get; set; }
 
             [Display(Name = "Workout_Description")]
-            public string Description { get; set; }
+            public string Workout_Description { get; set; }
 
             [Display(Name = "Created")]
             public DateTimeOffset CreatedUtc { get; set; }
