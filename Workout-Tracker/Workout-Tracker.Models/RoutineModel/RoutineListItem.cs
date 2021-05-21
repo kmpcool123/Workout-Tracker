da560e33996250cddf986265d869d1963a502639
Kayla-Kirk <Therainbowbitme@gmail.com>
2021-05-20 20:01:28 -0400

    RoutineListItem

---
+++ Workout-Tracker/Workout-Tracker.Models/RoutineModel/RoutineListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_Tracker.Models.RoutineModel
+{
+    public class RoutineListItem
+    {
+        public int RoutineId { get; set; }
+        public string RoutineName { get; set; }
+        public string RoutineDescription { get; set; }
+        public string WorkoutName { get; set; }
+        public string ExerciseName { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
--- Workout-Tracker/Workout-Tracker/Controllers/RoutineController.cs
+++ Workout-Tracker/Workout-Tracker/Controllers/RoutineController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using Workout_Tracker.Models.RoutineModel;
+using Workout_Tracker.Services;
 
 namespace Workout_Tracker.Controllers
 {
@@ -11,7 +15,7 @@ namespace Workout_Tracker.Controllers
     {
         private RoutineService CreateRoutineService()
         {
-            var userId = Guid.Parse(User.Identit.GetUserId());
+            var userId = Guid.Parse(User.Identity.GetUserId());
             var routineService = new RoutineService(userId);
             return routineService;
         }
